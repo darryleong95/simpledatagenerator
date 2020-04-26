8015f1470fc84d9ac1496d4b19b14b9d0f5a3b81
Darryl Leong <darryleong95@gmail.com>
2020-04-26 09:41:39 +0800

    Updates

---
+++ precache-manifest.a25a1b1706fd3636c6d95b656d36dd89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fbd6f3776f79b61543e7972fa69d32",
+    "url": "/simpledatagenerator/index.html"
+  },
+  {
+    "revision": "bc370f286fa71c01e3bc",
+    "url": "/simpledatagenerator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "95027b630e675f7e96d7",
+    "url": "/simpledatagenerator/static/js/2.819b065c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simpledatagenerator/static/js/2.819b065c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc370f286fa71c01e3bc",
+    "url": "/simpledatagenerator/static/js/main.3c572c44.chunk.js"
+  },
+  {
+    "revision": "54686ad0979d95fe065a",
+    "url": "/simpledatagenerator/static/js/runtime-main.6f75ee67.js"
+  }
+]);
